e8fa04323d172101386c49d3fa0cb66b590aca67
Om-Prasad-Sahu <omprasad.sahu@unifo.in>
2025-10-15 18:43:37 +0530

    CHANGES PORT NAMES IN FRONT END

--- Frontend/src/Config/config.js
+++ Frontend/src/Config/config.js
@@ -1,5 +1,5 @@
-const port = import.meta.env.VITE_PORT || 5000;
-const localHost = import.meta.env.VITE_HOST || "localhost";
+const port = import.meta.env.SERVER_PORT || 5000;
+const localHost = import.meta.env.SERVER_HOST || "localhost";
 const domain = `http://${localHost}:${port}`;
 
 export const config = { domain, localHost, port };
